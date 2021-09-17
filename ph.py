2216a9e97a389c7dd6b4ad416c957208b7520962
JerryyoP <73817441+JerryyoP@users.noreply.github.com>
2021-09-17 07:41:05 +0530

    Update ph.py

--- ph.py
+++ ph.py
@@ -1,10 +1,11 @@
-from pyrogram import Client, Filters
+from pyrogram import Client, Filters, idle
 from phlib import PornHub
 import os
 import asyncio
 loop = asyncio.get_event_loop()
 bot = Client("bot",api_id=6, api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e", bot_token="1986237766:AAFzyUrFmeNcBvCsXXn1b3ZvBmBYAlHicbQ"
 async def main():
+    await bot.start()
     os.system("mkdir Purn && cd Purn && ph thicc boobs --max=5 --download")
     videos = glob.glob("Purn/*.mp4")
     for y in videos:
@@ -12,6 +13,6 @@ async def main():
     await bot.send_message(-1001454433297, "Done 30")
     os.system("rm -r Purn")
     await bot.send_message(1825602460, "Done successfully")
-
+    await pyrogram.idle()
 if __name__ == "__main__":
     loop.run_until_complete(main())
